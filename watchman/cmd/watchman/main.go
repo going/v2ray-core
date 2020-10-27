8e68f3b2bb5b751bd71451e2336fe7196455a786
Tony.Shao <xiocode@gmail.com>
2020-10-27 14:05:12 +0800

    update

--- watchman/cmd/watchman/main.go
+++ watchman/cmd/watchman/main.go
@@ -7,14 +7,15 @@ import (
 )
 
 var (
-	address    = flag.String("address", "127.0.0.1:4321", "v2ray gRPC server address")
-	inboundTag = flag.String("tag", "vmess-proxy", "v2ray inbound tag name")
-	dbUrl      = flag.String("db", "222kingshard333:sddssds3322we@tcp(45.76.99.66:1795)/dotunnel005", "database address")
-	nodeId     = flag.Int64("node", 1, "node id")
+	address         = flag.String("address", "127.0.0.1:4321", "v2ray gRPC server address")
+	vmessInboundTag = flag.String("tag", "vmess-proxy", "v2ray inbound tag name")
+	vlessInboundTag = flag.String("tag", "vless-proxy", "v2ray inbound tag name")
+	dbUrl           = flag.String("db", "222kingshard333:sddssds3322we@tcp(108.160.132.5:1795)/ss2", "database address")
+	nodeId          = flag.Int64("node", 1, "node id")
 )
 
 func main() {
 	flag.Parse()
 
-	watchman.Start(*address, *inboundTag, *dbUrl, *nodeId)
+	watchman.Start(*address, *vmessInboundTag, *vlessInboundTag, *dbUrl, *nodeId)
 }
